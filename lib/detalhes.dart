954ede78d005632102004568abc7c7d494941593
Victor Ramos <ramos_victor@outlook.com>
2025-10-21 17:56:49 -0300

    Finalização de projeto

--- lib/detalhes.dart
+++ lib/detalhes.dart
@@ -1,26 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'resultadoBusca.dart';
+import 'dart:convert';
 
 class Detalhes extends StatefulWidget {
-  const Detalhes({super.key});
+
+  final int id;
+  const Detalhes(this.id,{super.key});
 
   @override
   State<Detalhes> createState() => _DetalhesState();
 }
 
 class _DetalhesState extends State<Detalhes> {
+
+  Map<String, dynamic> filme = {};
+  String apiKey = "";
+
+  Future<Map<String, dynamic>> dadosRecebidos() async{
+    var retorno = await http.get(Uri.parse("https://api.themoviedb.org/3/movie/${widget.id}?api_key=${apiKey}&language=pt-BR"));
+    filme = jsonDecode(retorno.body);
+    return filme;
+  }
+
+  void buscarFilme(){
+    dadosRecebidos().then((dados){
+      setState(() {
+        filme = dados;
+      });
+  });
+}
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    buscarFilme();
+  }
+
+  TextEditingController controller = TextEditingController();
+
+  void fazerBusca (){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => ResultadoBusca(controller.text)));
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (filme.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Carregando..."),
+          backgroundColor: Color(0xff14181c),
+        ),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    final urlImagem = filme["poster_path"];
+    final titulo = filme["title"];
+    final nota = filme["vote_average"];
+    final descricao = filme["overview"];
+    final orcamento = filme["budget"];
+    final bilheteria = filme["revenue"];
+
+    Widget imagemFilme() {
+      if (urlImagem != null && urlImagem.isNotEmpty && urlImagem != "null") {
+        return Image.network("https://image.tmdb.org/t/p/w500/$urlImagem",
+          height: 500,
+          width: double.infinity,
+          fit: BoxFit.cover
+        );
+      } else {
+        return Image.asset(
+          "imagens/sem-imagem.jpg",
+          height: 500,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        );
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
           padding: EdgeInsets.all(10),
           child: Image.asset("imagens/icone.png"),
         ),
-        title: Text("Buscador de filmes",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16
-          ),
+        title: Text(
+          "Buscador de filmes",
+          style: TextStyle(color: Colors.white, fontSize: 16),
         ),
         backgroundColor: Color(0xff14181c),
         actions: [
@@ -28,6 +95,7 @@ class _DetalhesState extends State<Detalhes> {
             width: 100,
             height: 45,
             child: TextField(
+              controller: controller,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
@@ -35,9 +103,9 @@ class _DetalhesState extends State<Detalhes> {
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(10),
-                    bottomLeft: Radius.circular(10)
-                  )
-                )
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
               ),
             ),
           ),
@@ -48,61 +116,111 @@ class _DetalhesState extends State<Detalhes> {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10)
-                )
-              )
-            ),
-            onPressed: (){},
-            child: Text("Buscar",
-              style: TextStyle(
-                color: Colors.white
+                  bottomRight: Radius.circular(10),
+                ),
               ),
             ),
-          )
+            onPressed: fazerBusca,
+            child: Text(
+              "Buscar",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ],
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
         color: Color(0xff14181c),
         child: Center(
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 70),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 3
+              Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(16),
+                        backgroundColor: Color(0xff00c030),
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Icon(Icons.arrow_back),
                     ),
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xff14181c)
                   ),
-                  width: 300,
-                  height: 500,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(17),
-                      bottom: Radius.circular(17)
-                    ),
-                    child: Image.asset(
-                      "imagens/sem-imagem.jpg",
-                      height: 494,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  )
-                ),
+                ],
               ),
-              Padding(padding: EdgeInsets.only(top: 15), child: Text("Batman: O Cavaleiro das Trevas", textAlign: TextAlign.justify, style: TextStyle(color: Colors.white))),
-              Padding(padding: EdgeInsets.only(top: 10), child: Text("Nota TMDB: 8.524 / 10", textAlign: TextAlign.justify, style: TextStyle(color: Colors.white))),
-              Padding(padding: EdgeInsets.only(top: 10), child: Text("Bilheteria: U\$ 185.000.000", textAlign: TextAlign.justify, style: TextStyle(color: Colors.white))),
-              Padding(padding: EdgeInsets.only(top: 10), child: Text("Custo: U\$ 1.004.558.444", textAlign: TextAlign.justify, style: TextStyle(color: Colors.white))),
-              Padding(padding: EdgeInsets.only(top: 10, right: 15, left: 15), child: Text("Sinopse: Após dois anos desde o surgimento do Batman, os criminosos de Gotham City têm muito o que temer. Com a ajuda do tenente James Gordon e do promotor público Harvey Dent, Batman luta contra o crime organizado. Acuados com o combate, os chefes do crime aceitam a proposta feita pelo Coringa e o contratam para combater o Homem-Morcego.", textAlign: TextAlign.justify, style: TextStyle(color: Colors.white)))
-            ],
+              Padding(
+                padding: EdgeInsets.only(top: 25),
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 3),
+                          borderRadius: BorderRadius.circular(20),
+                          color: Color(0xff14181c),
+                        ),
+                        width: 300,
+                        height: 500,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.vertical(
+                            top: Radius.circular(17),
+                            bottom: Radius.circular(17),
+                          ),
+                          child: imagemFilme(),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: Text(
+                          titulo,
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Nota TMDB: ${nota.toStringAsFixed(1)}",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Bilheteria: U\$ ${bilheteria.toString()}",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "Custo: U\$ ${orcamento.toString()}",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10, right: 15, left: 15),
+                        child: Text(
+                          "Sinopse: $descricao",
+                          textAlign: TextAlign.justify,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
+        )
+      )
     );
   }
 }
-
--- lib/paginaInicial.dart
+++ lib/paginaInicial.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'detalhes.dart';
+import 'resultadoBusca.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class PaginaInicial extends StatefulWidget {
   const PaginaInicial({super.key});
@@ -8,8 +12,47 @@ class PaginaInicial extends StatefulWidget {
 }
 
 class _PaginaInicialState extends State<PaginaInicial> {
+
+  Map<String, dynamic> filmes = {};
+  String apiKey = "";
+
+  Future<Map<String, dynamic>> dadosRecebidos() async {
+    var retorno = await http.get(Uri.parse("https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1"));
+    filmes = jsonDecode(retorno.body);
+    return filmes;
+  }
+
+  void buscarFilme(){
+    dadosRecebidos().then((dados){
+      setState(() {
+        filmes = dados;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    buscarFilme();
+  }
+
+  void verDetalhes(id){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Detalhes(id)));
+  }
+
+  TextEditingController controller = TextEditingController();
+
+  void fazerBusca (){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => ResultadoBusca(controller.text)));
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    if (filmes == null || filmes['results'] == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
@@ -28,6 +71,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
             width: 100,
             height: 45,
             child: TextField(
+              controller: controller,
               decoration: InputDecoration(
                 filled: true,
                 fillColor: Colors.white,
@@ -52,7 +96,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
                 )
               )
             ),
-            onPressed: (){},
+            onPressed: fazerBusca,
             child: Text("Buscar",
               style: TextStyle(
                 color: Colors.white
@@ -61,55 +105,82 @@ class _PaginaInicialState extends State<PaginaInicial> {
           )
         ],
       ),
-      body: Container(
-        color: Color(0xff14181c),
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 70),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white,
-                      width: 3
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xff14181c)
-                  ),
-                  width: 300,
-                  height: 500,
-                  child: Column(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(17),
-                        ),
-                        child: Image.asset(
-                          "imagens/sem-imagem.jpg",
-                          height: 400,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Padding(
-                          padding: EdgeInsets.only(top: 15),
-                          child: Text(
-                            "Teste 1: A Missão",
-                            style: TextStyle(color: Colors.white),
+      body: ListView.builder(
+        itemCount: filmes["results"].length,
+        itemBuilder: (context, index){
+
+          final filme = filmes["results"][index];
+          final urlImagem = filme["poster_path"];
+          final titulo = filme["title"];
+          final id = filme["id"];
+
+          return Container(
+            color: Color(0xff14181c),
+            child: Center(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 70),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white,
+                            width: 3
                           ),
-                        )
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xff14181c)
                       ),
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+                      width: 300,
+                      height: 550,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.vertical(
+                              top: Radius.circular(17),
+                            ),
+                            child: Image.network(
+                              "https://image.tmdb.org/t/p/w400/$urlImagem",
+                              height: 400,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Padding(padding: EdgeInsets.only(top: 15),
+                              child: Text(titulo,
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            )
+                          ),
+                          ElevatedButton(
+                            onPressed: (){
+                              verDetalhes(id);
+                            },
+                            child: Text("Ver detalhes",
+                              style: TextStyle(
+                                color: Colors.white
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(30, 35),
+                              backgroundColor: Color(0xff00c030),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              )
+                            )
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      )
     );
   }
 }
